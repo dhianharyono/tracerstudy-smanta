baf751404f5aac92eadc9267a75b6df0f50180b6
dhianharyono <dhianharyono.dh@gmail.com>
2025-12-16 15:50:00 +0700

    activate analytic

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -20,7 +20,7 @@ import {
   // FaSpinner,
 } from 'react-icons/fa';
 import './Layout.css';
-// import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const Layout = () => {
   const { user, logout } = useAuth();
@@ -349,7 +349,7 @@ const Layout = () => {
               </div>
             )} */}
             <Outlet />
-            {/* <Analytics /> */}
+            <Analytics />
           </div>
         </main>
       </div>
